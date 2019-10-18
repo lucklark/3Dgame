a1d2c5f987c47ad327934410177bc3ce9fb84291
13402977489 <517707320@qq.com>
2019-10-18 21:34:51 +0800

    homework5

---
+++ homework5/Assets/Scripts/PhysisFlyActionManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysisFlyActionManager : SSActionManager {
+
+	public PhysisUFOFlyAction fly;
+
+	// Use this for initialization
+	protected void Start () {
+		
+	}
+	
+
+	// 飞碟飞行
+	public void UFOFly(GameObject disk, float angle, float power) {
+		fly = PhysisUFOFlyAction.GetSSAction(disk.GetComponent<DiskData>().direction, angle, power);
+		this.RunAction(disk, fly, this);
+	}
+}
---
+++ homework5/Assets/Scripts/PhysisUFOFlyAction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysisUFOFlyAction : SSAction {
+
+	private Vector3 start_vector;                              //初速度向量
+	public float power;
+	private PhysisUFOFlyAction() {
+
+	}
+	
+	public static PhysisUFOFlyAction GetSSAction(Vector3 direction, float angle, float power) {
+		//初始化物体将要运动的初速度向量
+		PhysisUFOFlyAction action = CreateInstance<PhysisUFOFlyAction>();
+		if (direction.x == -1) {
+			action.start_vector = Quaternion.Euler(new Vector3(0, 0, -angle)) * Vector3.left * power;
+		} else {
+			action.start_vector = Quaternion.Euler(new Vector3(0, 0, angle)) * Vector3.right * power;
+		}
+		action.power = power;
+		return action;
+	}
+
+	public override void FixedUpdate() {
+		//判断是否超出范围
+		if (this.transform.position.y < -10) {
+			this.destroy = true;
+			this.callback.SSActionEvent(this);
+		}
+	}
+
+	// Use this for initialization
+	public override void Start () {
+		
+	}
+	
+
+	// Update is called once per frame
+	public override void Update () {
+		//使用重力以及给一个初速度
+		gameobject.GetComponent<Rigidbody>().velocity = power / 35 * start_vector;
+		gameobject.GetComponent<Rigidbody>().useGravity = true;
+	}
+}
