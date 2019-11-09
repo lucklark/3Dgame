181cdac8aa58a28ab49eec345bf3968dbd1479bb
13402977489 <517707320@qq.com>
2019-11-09 20:58:29 +0800

    hw7

---
+++ homework7/HaloParticle/Assets/Scripts/HaloParticle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HaloParticle : MonoBehaviour
+{
+    public float radius, angle, time;
+    public HaloParticle(float radius_, float angle_, float time_)
+    {
+        radius = radius_; // 粒子的分布半径
+        angle = angle_; // 粒子的和水平轴成的角度，本实验中单位均为弧度
+        time = time_; // 时间，主要用于关联后期粒子的来回周期性运动
+    }
+}
---
+++ homework7/HaloParticle/Assets/Scripts/Inner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inner : MonoBehaviour
+{
+    public ParticleSystem ps; // 粒子系统对象
+    private ParticleSystem.Particle[] particle_arr; // 存储粒子的仓库
+    private int resolution = 3000; // 最大粒子数
+    private HaloParticle[] hp; // 存储每一个粒子位置信息的数组
+    // 内环的粒子总体分布半径要小一些
+    private float min_radius = 2F;
+    private float max_radius = 4F;
+    private float pingPong = 0.01F; // 粒子在来回运动的时候的游离范围
+
+    void Start()
+    {
+        ps = this.GetComponent<ParticleSystem>();
+        particle_arr = new ParticleSystem.Particle[resolution];
+        hp = new HaloParticle[resolution];
+        ps.Emit(resolution);
+        ps.GetParticles(particle_arr);
+        // 初始化各个粒子的位置
+        for (int i = 0; i < resolution; i++)
+        {
+            // 使得粒子集中在平均半径处
+            float shiftMinRadius = Random.Range(1, (min_radius + max_radius) / (2 * min_radius));
+            float shiftMaxRadius = Random.Range((min_radius + max_radius) / (2 * max_radius), 1);
+            //粒子角度,添加一个偏移量，使粒子集中于π/4和5π/4处  
+            float shiftMinAngle = Random.Range(1, 1.5f);
+            float shiftMaxAngle = Random.Range(0.75f, 1);
+            // 粒子的角度为[0, 2π], 按照平面直角坐标系的参数方程进行设定
+            float angle = Random.Range(Mathf.PI * shiftMinAngle, Mathf.PI * 2 * shiftMaxAngle) - Mathf.PI / 4;
+            if (Random.Range(0, 100) > 50) angle -= Mathf.PI;
+
+            float radius = Random.Range(min_radius * shiftMinRadius, max_radius * shiftMaxRadius);
+            float time = Random.Range(0.0f, 360.0f);
+            hp[i] = new HaloParticle(radius, angle, time);
+            particle_arr[i].position = new Vector3(radius * Mathf.Cos(angle), radius * Mathf.Sin(angle), 0);
+        }
+        ps.SetParticles(particle_arr, particle_arr.Length);
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < resolution; i++)
+        {
+            // 使粒子旋转
+            hp[i].angle -= Random.Range(0, 1F / 360);
+            particle_arr[i].position = new Vector3(hp[i].radius * Mathf.Cos(hp[i].angle), hp[i].radius * Mathf.Sin(hp[i].angle), 0);
+            // 调整粒子上下浮动
+            hp[i].time += Time.deltaTime;
+            hp[i].radius += Mathf.PingPong(hp[i].time / min_radius / max_radius, pingPong) - pingPong / 2.0f;
+        }
+        ps.SetParticles(particle_arr, particle_arr.Length);
+    }
+}
